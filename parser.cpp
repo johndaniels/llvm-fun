4d55cd11d992995bb790a9d85cdab004b01d9b4e
John Daniels <johnsdaniels@gmail.com>
2013-03-17 19:25:43 -0400

    More changes to parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -20,54 +20,61 @@ namespace lang {
 	};
 
 	struct IdToken : Token {
-		string id;
-		IdToken(shared_ptr<vector<char> > value) : id(value->begin(), value->end()) {}
+		shared_ptr<string> id;
+		IdToken(shared_ptr<string> value) : id(value) {}
 		virtual TokenType getType() {
 			return ID;
 		}
 	};
 
-	struct StringIterator {
+	template <class T> struct Iterator {
+		virtual T value() = 0;
+		virtual bool hasValue() = 0;
+		virtual shared_ptr<Iterator<T> > next() = 0;
+		virtual size_t getPosition() = 0;
+	};
+
+	struct StringIterator : Iterator<char> {
 		StringIterator(const string& text) {
 			this->position = 0;
 			this->text = shared_ptr<string>(new string(text));
 		}
 
-		size_t position;
-		shared_ptr<string> text;
-
-		char value() {
+		virtual char value() {
 			if (!hasValue()) {
 				return 0;
 			}
 			return (*text)[position];
 		}
 
-		bool hasValue() {
+		virtual bool hasValue() {
 			return position < text->size();
 		}
 
-		StringIterator next() {
-			return StringIterator(position + 1, text);
+		virtual shared_ptr<Iterator<char> > next() {
+			return shared_ptr<Iterator<char> > (new StringIterator(position + 1, text));
 		}
 
-		bool operator> (const StringIterator& other) {
-			return position > other.position;
+		virtual size_t getPosition() {
+			return this->position;
 		}
 
 	private:
+		size_t position;
+		shared_ptr<string> text;
+
 		StringIterator(size_t pos, shared_ptr<string> text) {
 			this->position = pos;
 			this->text = text;
 		}
 	};
 
-	template <class Iterator, class T> struct ParseResult {
-		ParseResult(Iterator iterator, T value) : iterator(iterator) {
+	template <class IteratorT, class T> struct ParseResult {
+		ParseResult(shared_ptr<Iterator<IteratorT> > iterator, T value) : iterator(iterator) {
 			this->success = true;
 			this->value = value;
 		}
-		ParseResult(Iterator iterator) : iterator(iterator) {
+		ParseResult(shared_ptr<Iterator<IteratorT> > iterator) : iterator(iterator) {
 			this->success = false;
 		}
 
@@ -79,83 +86,81 @@ namespace lang {
 			}
 		}
 
-		Iterator iterator;
+		shared_ptr<Iterator<IteratorT> > iterator;
 		bool success;
 		T value;
 	};
 
-	template <class Iterator, class T>
+	template <class InT, class OutT>
 	struct Parser {
-		virtual ParseResult<Iterator, T> parse(Iterator it) = 0;
+		virtual ParseResult<InT, OutT> parse(shared_ptr<Iterator<InT> > it) = 0;
 	};
 
-	template <class Iterator>
-	struct CharParser : Parser<Iterator, char> {
+	struct CharParser : Parser<char, char> {
 		char character;
 		CharParser(char character) {
 			this->character = character;
 		}
 
-		virtual ParseResult<Iterator, char> parse(Iterator it) {
-			if (!it.hasValue() || it.value() != character) {
-				return get_failure(it, '\0');
+		virtual ParseResult<char, char> parse(shared_ptr<Iterator<char> > it) {
+			if (!it->hasValue() || it->value() != character) {
+				return ParseResult<char, char>(it, '\0');
 			}
-			return get_success(it.next(), this->character);
+			return ParseResult<char, char>(it->next(), this->character);
 		}
 	};
 
-	template <class Iterator, class T>
-	struct OrParser : Parser<Iterator, T> {
-		shared_ptr<vector<shared_ptr<Parser<Iterator, T> > > > parsers;
-		OrParser(shared_ptr<vector<shared_ptr<Parser<Iterator, T> > > > parsers) {
+	template <class InT, class OutT>
+	struct OrParser : Parser<InT, OutT> {
+		shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > > parsers;
+		OrParser(shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > > parsers) {
 			this->parsers = parsers;
 		}
 
-		OrParser(Parser<Iterator, T> a, Parser<Iterator, T> b) : parsers(new vector<shared_ptr<Parser<Iterator, T> > >) {
+		OrParser(shared_ptr<Parser<InT, OutT> > a, shared_ptr<Parser<InT, OutT> > b) : parsers(new vector<shared_ptr<Parser<InT, OutT> > >) {
 			this->parsers.push_back(a);
 			this->parsers.push_back(b);
 		}
 
-		virtual ParseResult<Iterator, T> parse(Iterator it) {
+		virtual ParseResult<InT, OutT> parse(shared_ptr<Iterator<InT> > it) {
 			for (size_t i=0; i<parsers->size(); i++) {
-				ParseResult<Iterator, T> result = (*parsers)[i]->parse(it);
+				ParseResult<InT, OutT> result = (*parsers)[i]->parse(it);
 				if (result.success) {
 					return result;
 				}
 			}
-			return ParseResult<Iterator, T>(it);
+			return ParseResult<InT, OutT>(it);
 		}
 
 	protected:
 		OrParser() {
-			this->parsers = shared_ptr<vector<shared_ptr<Parser<Iterator, T> > > >(new vector<shared_ptr<Parser<Iterator, T> > >);
+			this->parsers = shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > >(new vector<shared_ptr<Parser<InT, OutT> > >);
 		}
 	};
 
-	template <class Iterator>
-	struct CharClassParser : OrParser<Iterator, char>{
+	struct CharClassParser : OrParser<char, char>{
 		CharClassParser(const string& text) {
 			for (size_t i=0; i<text.size(); i++) {
-				shared_ptr<CharParser<Iterator> > charParser(new CharParser<Iterator>(text[i]));
+				shared_ptr<CharParser> charParser(new CharParser(text[i]));
 				this->parsers->push_back(charParser);
 			}
 		}
 	};
 
-	template <class Iterator, class T, class ParserT, class SeqT>
-	struct SequenceParser : Parser<Iterator, shared_ptr<SeqT> > {
-		shared_ptr<vector<shared_ptr<ParserT> > > parsers;
-		SequenceParser(shared_ptr<vector<shared_ptr<ParserT> > > parsers) {
+	template <class InT, class OutT>
+	struct SequenceParser : Parser<InT, shared_ptr<vector<OutT> > > {
+		shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > > parsers;
+		SequenceParser(shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > > parsers) {
 			this->parsers = parsers;
 		}
 
-		virtual ParseResult<Iterator, shared_ptr<SeqT> > parse(Iterator it) {
-			Iterator start = it;
-			ParseResult<Iterator, shared_ptr<SeqT> > totalResult(it, shared_ptr<SeqT>(new SeqT()));
+		virtual ParseResult<InT, shared_ptr<vector<OutT> > > parse(shared_ptr<Iterator<InT> > it) {
+			shared_ptr<Iterator<InT> > start = it;
+			ParseResult<InT, shared_ptr<vector<OutT> > > totalResult(it, shared_ptr<vector<OutT> >(new vector<OutT>()));
 			for (size_t i=0; i<parsers->size(); i++) {
-				ParseResult<Iterator, T> result = (*parsers)[i].parse(totalResult.iterator);
+				ParseResult<InT, OutT> result = (*parsers)[i].parse(totalResult.iterator);
 				if (!result.success) {
-					return ParseResult<Iterator, shared_ptr<SeqT> >(start);
+					return ParseResult<InT, shared_ptr<vector<OutT> > >(start);
 				}
 				totalResult.iterator = result.iterator;
 				totalResult.value->push_back(result.value);
@@ -164,16 +169,16 @@ namespace lang {
 		}
 	};
 
-	template <class Iterator, class T>
-	struct StarParser : Parser<Iterator, shared_ptr<vector<T> > > {
-		shared_ptr<Parser<Iterator, T> > parser;
-		StarParser(shared_ptr<Parser<Iterator, T> > parser) : parser(parser) {
+	template <class InT, class OutT>
+	struct StarParser : Parser<InT, shared_ptr<vector<OutT> > > {
+		shared_ptr<Parser<InT, OutT> > parser;
+		StarParser(shared_ptr<Parser<InT, OutT> > parser) : parser(parser) {
 		}
 
-		virtual ParseResult<Iterator, shared_ptr<vector<T> > > parse(Iterator it) {
-			ParseResult<Iterator, shared_ptr<vector<T> > > totalResult(it, shared_ptr<vector<T> >(new vector<T>()));
+		virtual ParseResult<InT, shared_ptr<vector<OutT> > > parse(shared_ptr<Iterator<InT> > it) {
+			ParseResult<InT, shared_ptr<vector<OutT> > > totalResult(it, shared_ptr<vector<OutT> >(new vector<OutT>()));
 			while (true) {
-				ParseResult<Iterator, T> result = parser->parse(totalResult.iterator);
+				ParseResult<InT, OutT> result = parser->parse(totalResult.iterator);
 				if (!result.success) {
 					return totalResult;
 				}
@@ -183,18 +188,18 @@ namespace lang {
 		}
 	};
 
-	template <class Iterator, class T>
-	struct LongestOrParser : Parser<Iterator, T> {
-		shared_ptr<vector<shared_ptr<Parser<Iterator, T>> > > parsers;
-		LongestOrParser(shared_ptr<vector<shared_ptr<Parser<Iterator, T> > > > parsers) {
+	template <class InT, class OutT>
+	struct LongestOrParser : Parser<InT, OutT> {
+		shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > > parsers;
+		LongestOrParser(shared_ptr<vector<shared_ptr<Parser<InT, OutT> > > > parsers) {
 			this->parsers = parsers;
 		}
 
-		virtual ParseResult<Iterator, T> parse(Iterator it) {
-			ParseResult<Iterator, T> best = ParseResult<Iterator, T>(it);
+		virtual ParseResult<InT, OutT> parse(shared_ptr<Iterator<InT> > it) {
+			ParseResult<InT, OutT> best(it);
 			for (size_t i=0; i<parsers->size(); i++) {
-				ParseResult<Iterator, T> result = (*parsers)[i]->parse(it);
-				if (result.success && result.iterator > best.iterator) {
+				ParseResult<InT, InT> result = (*parsers)[i]->parse(it);
+				if (result.success && result.iterator->getPosition() > best.iterator->getPosition()) {
 					best = result;
 				}
 			}
@@ -202,50 +207,44 @@ namespace lang {
 		}
 	};
 
-	template <class Iterator, class InT, class OutT>
-	struct TokenParser : Parser<Iterator, shared_ptr<Token> > {
-		shared_ptr<Parser<Iterator, InT> > parser;
-		TokenParser(shared_ptr<Parser<Iterator, InT> > parser) : parser(parser) {}
-		virtual ParseResult<Iterator, shared_ptr<Token> > parse(Iterator it) {
-			ParseResult<Iterator, InT> result = parser->parse(it);
+	template <class OutT>
+	struct TokenParser : Parser<char, shared_ptr<Token> > {
+		shared_ptr<Parser<char, shared_ptr<vector<char> > > > parser;
+		TokenParser(shared_ptr<Parser<char, shared_ptr<vector<char> > > > parser) : parser(parser) {}
+		virtual ParseResult<char, shared_ptr<Token> > parse(shared_ptr<Iterator<char> > it) {
+			ParseResult<char, shared_ptr<vector<char> > > result = parser->parse(it);
 			if (result.success) {
-				return ParseResult<Iterator, shared_ptr<Token> >(it, shared_ptr<Token>(new OutT(result.value)));
+				shared_ptr<string> text(new string(result.value->begin(), result.value->end()));
+				return ParseResult<char, shared_ptr<Token> >(it, shared_ptr<Token>(new OutT(text)));
 			}
-			return ParseResult<Iterator, shared_ptr<Token> >(it);
+			return ParseResult<char, shared_ptr<Token> >(it);
 		}
 	};
 
-	template <class Iterator, class OutT> shared_ptr<Parser<Iterator, shared_ptr<Token> > > tokenParser(shared_ptr<Parser<Iterator, shared_ptr<vector<char> > > > input) {
-		return shared_ptr<Parser<Iterator, shared_ptr<Token> > >(new TokenParser<Iterator, shared_ptr<vector<char> >, OutT >(input));
-	}
-
-	template <class Iterator, class T> ParseResult<Iterator, T> get_success(Iterator iterator, T value) {
-		return ParseResult<Iterator, T>(iterator, value);
-	}
-
-	template <class Iterator, class T> ParseResult<Iterator, T> get_failure(Iterator iterator, T value) {
-		return ParseResult<Iterator, T>(iterator);
-	}
-
-	template <class Iterator, class T> shared_ptr<Parser<Iterator, T> > orParser(shared_ptr<Parser<Iterator, T> > a, shared_ptr<Parser<Iterator, T> > b) {
-		return shared_ptr<Parser<Iterator, T> >(new OrParser<Iterator, T>(a, b));
-	}
-
-	template <class Iterator, class T> shared_ptr<Parser<Iterator, shared_ptr< vector<T> > > > star(shared_ptr<Parser<Iterator, T> > parser) {
-		return shared_ptr<Parser<Iterator, shared_ptr< vector<T> > > >(new StarParser<Iterator, T>(parser));
-	}
-
 	template <class T> shared_ptr<T> smartify(T* ptr) {
 		return shared_ptr<T>(ptr);
-	}		
+	}
+
+	namespace tokenizer {
+		template <class OutT> shared_ptr<Parser<char, shared_ptr<Token> > > token_parser(shared_ptr<Parser<char, shared_ptr<vector<char> > > > input) {
+			return shared_ptr<Parser<char, shared_ptr<Token> > >(new TokenParser< OutT >(input));
+		}
+
+		template <class OutT> shared_ptr<Parser<char, shared_ptr< vector<OutT> > > > star(shared_ptr<Parser<char, OutT> > parser) {
+			return shared_ptr<Parser<char, shared_ptr< vector<OutT> > > >(new StarParser<char, OutT>(parser));
+		}
+
+		shared_ptr<vector<shared_ptr<Token> > > > tokenize() {
+			shared_ptr<CharClassParser> id_char_parser(new CharClassParser("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"));
+			auto id_parser = token_parser<IdToken>(star<char>(id_char_parser));
+			auto multi_whitespace_parser = tokenParser<IdToken>(star<char>(smartify(new CharClassParser(" \t\n\r"))));
+		}
+	}
 
 	typedef StarParser<StringIterator, char> StarCharClassParser;
 	void do_parse() {
-
-
-		shared_ptr<Parser<StringIterator, char>> parser(new CharClassParser<StringIterator>("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"));
-		auto id_parser = tokenParser<StringIterator, IdToken>(star<StringIterator, char>(parser));
-		auto multi_whitespace_parser = tokenParser<StringIterator, IdToken>(star<StringIterator, char>(smartify(new CharClassParser<StringIterator>(" \t\n\r"))));
+		
+		/*
 
 		shared_ptr<vector<shared_ptr<Parser<StringIterator, shared_ptr<Token> > > > > tokenParserList(new vector<shared_ptr<Parser<StringIterator, shared_ptr<Token> > > >);
 		tokenParserList->push_back(id_parser);
@@ -257,6 +256,7 @@ namespace lang {
 		
 		auto result = tokenParser->parse(it);
 		//cout << *result.value << endl;
+		*/
 	}
 
 	shared_ptr<Ast> parse() {
